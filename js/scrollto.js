7074dd5adafb584dec776b57c23a345ca5051f5a
Norbert Kuemin <norbert@kuem.in>
2017-07-04 16:08:58 +0100

    Slow down scrolling

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,6 +1,6 @@
 function scrollToAnchor(aid) {
 	var aTag = jQuery("a[name='"+ aid +"']");
 	var body = jQuery("html, body");
-	body.animate({scrollTop: aTag.offset().top},'slow');
+	body.animate({scrollTop: aTag.offset().top},1000);
 }
 
